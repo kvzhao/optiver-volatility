d658517f8ed2c54c345342ac8ead774e66353cc6
kv <kelispinor@gmail.com>
2021-07-02 15:13:32 +0800

    add samples for eda

--- code/quick_eqa.py
+++ code/quick_eqa.py
@@ -1,4 +1,9 @@
 
+"""
+https://www.kaggle.com/piantic/starter-optiver-quick-eda-automl-wip
+
+"""
+
 import os
 import random
 import glob
@@ -15,7 +20,79 @@ import pyarrow.parquet as pq
 import plotly.express as px
 import plotly.graph_objects as go
 
+from utils import calculate_wap, log_return
+from utils import realized_volatility
 
-book_example = pd.read_parquet('../input/optiver-realized-volatility-prediction/book_train.parquet/stock_id=0')
+dataset = pq.ParquetDataset('../input/optiver-realized-volatility-prediction/book_train.parquet/stock_id=0')
+book_example = dataset.read()
+book_example = book_example.to_pandas()
+
+stock_id = '5'
+time_id = 5
+book_example = book_example[book_example['time_id']==time_id]
+book_example.loc[:,'stock_id'] = stock_id
+
+msno.matrix(book_example, fontsize = 16)
+
+book_example['wap'] = calculate_wap(book_example)
+book_example.loc[:,'log_return'] = log_return(book_example['wap'])
+book_example = book_example[~book_example['log_return'].isnull()]
+
+book_example.loc[:,'realized_volatility'] = book_example.groupby(['time_id'])['log_return'].apply(realized_volatility)
+new_book_example = book_example[~book_example['realized_volatility'].isnull()].reset_index()
 
 print(book_example)
+
+print(new_book_example)
+
+pd.DataFrame(book_example.groupby(['time_id'])['log_return'].agg(realized_volatility)).reset_index()
+
+temp_aggs = book_example.groupby(['time_id']).agg(
+                                            bid_price1_min = ('bid_price1', 'min'),
+                                            bid_price2_max = ('bid_price2', 'max'),
+                                            bid_price1_mean = ('bid_price1', 'mean'),
+                                            bid_price2_mean = ('bid_price2', 'mean'),
+                                            bid_price1_median = ('bid_price1', 'median'),
+                                            bid_price2_median = ('bid_price2', 'median'),
+                                            ask_price1_min = ('ask_price1', 'min'),
+                                            ask_price2_max = ('ask_price2', 'max'),
+                                            ask_price1_mean = ('ask_price1', 'mean'),
+                                            ask_price2_mean = ('ask_price2', 'mean'),
+                                            ask_price1_median = ('ask_price1', 'median'),
+                                            ask_price2_median = ('ask_price2', 'median'),
+)
+
+aggs_book_example = pd.merge(new_book_example, temp_aggs, on=['time_id'], how='left')
+print(aggs_book_example)
+
+
+#fig = px.line(book_example, x="seconds_in_bucket", y="wap", title='WAP of stock_id_5, time_id_5')
+#fig.show()
+#fig = px.line(book_example, x="seconds_in_bucket", y="log_return", title='Log return of stock_id_5, time_id_5')
+#fig.show()
+
+
+"""
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(
+        x=book_example["seconds_in_bucket"], 
+        y=book_example["bid_price1"], 
+        mode='lines', 
+        name='bid_price1'
+    )
+)
+fig.add_trace(
+    go.Scatter(
+        x=book_example["seconds_in_bucket"], 
+        y=book_example["bid_price2"], 
+        mode='lines', 
+        name='bid_price2'
+    )
+)
+
+fig.show()
+
+"""
--- code/utils.py
+++ code/utils.py
@@ -14,20 +14,17 @@ def log_return(list_stock_prices):
 
 
 def realized_volatility(series_log_return):
-    return np.sqrt(np.sum(series_log_return**2))
+    return np.sqrt(np.sum(series_log_return ** 2))
 
 
 def calculate_wap(df):
     '''
     https://www.kaggle.com/konradb/we-need-to-go-deeper
     '''
-#     a = df['bid_price1'] * df['ask_size1'] + df['ask_price1'] * df['bid_size1']
-#     b = df['bid_size1']+ df['ask_size1']
-        
     a1 = df['bid_price1'] * df['ask_size1'] + df['ask_price1'] * df['bid_size1']
     a2 = df['bid_price2'] * df['ask_size2'] + df['ask_price2'] * df['bid_size2']
     b = df['bid_size1'] + df['ask_size1'] + df['bid_size2']+ df['ask_size2']
-    
+
     x = (a1 + a2) / b
     return x
 
@@ -37,17 +34,17 @@ def get_log_return_df_per_time_id(file_path):
     dataset = pq.ParquetDataset(file_path)
     book_dataset = dataset.read()
     df_book_data = book_dataset.to_pandas()
-    
+
     df_book_data['wap'] = calculate_wap(df_book_data)
     df_book_data['log_return'] = df_book_data.groupby(['time_id'])['wap'].apply(log_return)
     df_book_data = df_book_data[~df_book_data['log_return'].isnull()]
 
     stock_id = file_path.split('=')[1]
     df_book_data['row_id'] = df_book_data['time_id'].apply(lambda x:f'{stock_id}-{x}')
-    
+ 
     del dataset, book_dataset
     gc.collect()
-    
+
     return df_book_data
 
 
@@ -56,20 +53,20 @@ def get_realized_volatility_df_per_time_id(file_path):
     dataset = pq.ParquetDataset(file_path)
     book_dataset = dataset.read()
     df_book_data = book_dataset.to_pandas()
-    
+
     df_book_data['wap'] = calculate_wap(book_example)
     df_book_data['log_return'] = df_book_data.groupby(['time_id'])['wap'].apply(log_return)
     df_book_data = df_book_data[~df_book_data['log_return'].isnull()]
-    
+
     df_book_data['realized_volatility'] = df_book_data.groupby(['time_id'])['log_return'].apply(realized_volatility)
     df_book_data = df_book_data[~df_book_data['realized_volatility'].isnull()]
 
     stock_id = file_path.split('=')[1]
     df_book_data['row_id'] = df_book_data['time_id'].apply(lambda x:f'{stock_id}-{x}')
-    
+
     del dataset, book_dataset
     gc.collect()    
-    
+
     return df_book_data
 
 
